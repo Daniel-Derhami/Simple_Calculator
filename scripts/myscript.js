7d9dd9b8edebe35acce1d749107feba2affbdfff
Daniel-Derhami <daniel.derhami@gmail.com>
2023-02-11 21:35:31 +0100

    Other functions added

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,8 +1,24 @@
-/* global document */
 
-flag = 0;
+
+var flag = 0;
 
 function sqrt_function() {
-    flag += 1;
     cal.display.value = Math.sqrt(cal.display.value);
+  flag += 1;
+}
+
+function cos_function() {
+    cal.display.value = Math.cos(cal.display.value);
+    flag += 1;
+}
+
+function sin_function() {
+    cal.display.value = Math.sin(cal.display.value);
+    flag += 1;
+}
+
+function Resetfunction(cal) {
+    cal.display.value = "";
+
+    flag = 0;
 }
